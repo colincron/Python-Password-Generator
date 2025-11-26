550b84e4db41da521ecd5d465f209f78015ed8e4
colin <colint.cron@gmail.vcom>
2025-11-25 19:25:04 -0500

    Added copying password to clipboard and more exception handling

--- password_tool.py
+++ password_tool.py
@@ -1,5 +1,6 @@
 #! /bin/python3
 import random, math
+import pyperclip
 
 def password_generator(password_length):
     valid_character_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
@@ -11,14 +12,15 @@ def password_generator(password_length):
     for i in range(password_length):
         rand_int = math.ceil(random.random()*(len(valid_character_list)-1))
         rand_letter = valid_character_list[rand_int]
-        # print(valid_character_list[rand_int])
         temp_pass.append(rand_letter)
-        # print("Password:  " + str(temp_pass))
         password = "".join(temp_pass)
-    print(password)
+    pyperclip.copy(password)
+    print("Your password is: " + password + "\nYour password has been copied to your clipboard.")
 
 try:
     input_length = int(input("Enter password character length: "))
     password_generator(input_length)
 except ValueError:
     print("Please enter an integer next time. ")
+except pyperclip.PyperclipException:
+    print("Please install a copy/paste mechanism on your system.")
